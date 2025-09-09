c4905761cd14eb4beb0d8715116abe8a23c686ef
anoob <anoob@sina.cn>
2025-09-10 01:02:53 +0800

    init

---
+++ conanfile.py
@@ -0,0 +1,16 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeDeps, CMakeToolchain
+
+class LightMaker(ConanFile):
+    settings = "os", "build_type"
+
+    def requirements(self):
+        self.requires("openssl/1.1.1w")
+        self.requires("sqlite3/3.43.0")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False
+        tc.generate()
+        cd = CMakeDeps(self)
+        cd.generate()
